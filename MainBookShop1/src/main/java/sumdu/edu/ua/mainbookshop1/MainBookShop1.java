582add2fee4d80adbee2b890585bc83ffa46c1b5
33ZoyaTaranets <35394217+33ZoyaTaranets@users.noreply.github.com>
2022-12-08 12:07:56 +0200

    Added

---
+++ MainBookShop1/src/main/java/sumdu/edu/ua/mainbookshop1/MainBookShop1.java
@@ -0,0 +1,11 @@
+package sumdu.edu.ua.mainbookshop1;
+
+public class MainBookShop1 {
+
+    public static void main(String[] args) {
+        
+        Book book1=new Book(1,"Tolkien","Lord of the Rings");
+        System.out.println(book1.toSting());
+    }
+}
+
